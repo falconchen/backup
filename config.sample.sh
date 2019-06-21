efab25bf683a2ad2a4f1564f9b4aa2bea62bd7e9
falcon <falcon_chen@qq.com>
2019-06-21 13:22:00 +0800

    init project

---
+++ config.sample.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#配置文件
+
+
+TODAY=`date -I` # 获取当前日期
+#EXPIRE_DAYS=`date -d -7day +"%Y-%m-%d"` # 获取7天前的日期
+EXPIRE_DAYS=7 #保留备份数
+BACK_BASE_DIR=/home/backups/daily # 备份基本目录
+BACK_TODAY_DIR=$BACK_BASE_DIR/today # 当天备份存放的目录
+BACKUP_LOG=$BACK_BASE_DIR/daily_${TODAY}.log # 备份日志,存放于备份目录下
+
+declare -A BACKUP_FROM_DIRS
+BACKUP_FROM_DIRS=([a_uploads]='/var/www/html/a/uploads' [b_uploads]='/var/www/html/b/uploads/' [db_backups]='/var/www/dbbackup')
---
+++ daily_backup.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+
+#
+BASEDIR=$(dirname "$0")
+#echo "$BASEDIR"
+CONFIG=$BASEDIR/config.sh
+
+if [ ! -f $CONFIG ];then
+    echo ${CONFIG} not exist!
+    exit 2
+fi
+source $CONFIG
+
+
+#每日增量备份
+#备份upload和database
+
+#Create Today BackupDirectory
+if [ ! -d $BACK_BASE_DIR ];then
+    mkdir -p $BACK_BASE_DIR
+fi
+
+
+#Rename Yestodaybackup DIR
+if [ ! -d $BACK_TODAY_DIR ];
+then
+    mkdir -p $BACK_TODAY_DIR
+else
+    mv $BACK_TODAY_DIR $BACK_BASE_DIR/`date -r $BACK_TODAY_DIR "+%Y-%m-%d_%H%M%S"`
+    mkdir -p $BACK_TODAY_DIR
+fi
+echo "Start Time : `date +%F" "%H:%M:%S`" >> $BACKUP_LOG
+
+#backup files 
+for name in ${!BACKUP_FROM_DIRS[@]}; do 
+#printf "%s: %s\n" $name ${BACKUP_FROM_DIRS[$name]}; 
+cd ${BACKUP_FROM_DIRS[$name]}
+tar czvf $BACK_TODAY_DIR/$name.tgz -C ${BACKUP_FROM_DIRS[$name]} `find * -mtime -1 -type f -print` 1 >>$BACKUP_LOG 2>>$BACKUP_LOG
+echo "finishing backup directory ${BACKUP_FROM_DIRS[$name]} to ${name}.tgz" >> $BACKUP_LOG
+done
+
+echo "Stop Time : `date +%F" "%H:%M:%S`" >> $BACKUP_LOG
+
+
+#Delete old data
+echo "###############################################################" >> $BACKUP_LOG
+echo "Delete expire data." >> $BACKUP_LOG
+echo "Start Time : `date +%F" "%H:%M:%S`" >> $BACKUP_LOG
+find ${BACK_BASE_DIR} -mtime +${EXPIRE_DAYS} | xargs rm -rf
+echo "Stop Time : `date +%F" "%H:%M:%S`" >> $BACKUP_LOG
